1a1220207d7e1890362d592cac0c441f9bd749fb
izebiafe <afimiafidelis95@gmail.com>
2023-12-11 12:28:44 +0100

    add the model validation

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,16 +1,16 @@
 class Post < ApplicationRecord
-  belongs_to :author, class_name: 'User'
+  belongs_to :user, foreign_key: 'author_id', counter_cache: true
   has_many :comments
   has_many :likes
 
   def mostrecentcomments
-    comments.order(created_at: :desc).limit(5)
+     omments.where(post_id: self[:id]).order(created_at: :desc).limit(5)
   end
   after_create :incrementpostcounter
 
   private
 
   def incrementpostcounter
-    author.increment!(:posts_counter)
+   author.increment!(posts_counter: user.posts.size)
   end
 end
