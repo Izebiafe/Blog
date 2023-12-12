ed3f854859e50bb51bd0b5c2b7fea3e833eb755a
izebiafe <afimiafidelis95@gmail.com>
2023-12-12 13:04:04 +0100

    set validation

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,16 +1,19 @@
 class Post < ApplicationRecord
-  belongs_to :user, foreign_key: 'author_id', counter_cache: true
+  belongs_to :author, class_name: 'User'
   has_many :comments
   has_many :likes
+  validates :title, presence: true, length: { maximum: 250 }
+  validates :likes_count, presence: false, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :comments_count, presence: false, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
-  def mostrecentcomments
-    omments.where(post_id: self[:id]).order(created_at: :desc).limit(5)
+  def mostrecentcomments(limit = 5)
+    comments.where(post_id: self[:id]).order(created_at: :desc).limit(limit)
   end
-  after_create :incrementpostcounter
+  after_save :incrementpostcounter
 
   private
 
   def incrementpostcounter
-    author.increment!(posts_counter: user.posts.size)
+    author.update(posts_count: author.posts.size)
   end
 end
--- db/migrate/20231212065408_create_posts.rb
+++ db/migrate/20231212065408_create_posts.rb
@@ -3,8 +3,9 @@ class CreatePosts < ActiveRecord::Migration[7.1]
     create_table :posts do |t|
       t.string :title
       t.text :text
-      t.integer :comments_counter
-      t.integer :likes_counter
+      t.integer :comments_count
+      t.integer :likes_count
+      t.references :author, foreign_key: { to_table: :users }, index: true
 
       t.timestamps
     end
