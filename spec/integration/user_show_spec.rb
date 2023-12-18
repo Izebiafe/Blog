24e1af7856bd214d212f29cae34deb7a9f23fcca
izebiafe <afimiafidelis95@gmail.com>
2023-12-18 14:49:21 +0100

    Add the integration tests

--- spec/integration/user_show_spec.rb
+++ spec/integration/user_show_spec.rb
@@ -5,11 +5,11 @@ RSpec.describe 'userpostpage#show', type: :feature do
                         photo: 'https://res.cloudinary.com/dsfuiu63q/image/upload/v1678870006/avatars/icon-256x256_wa30f0.png',
                         bio: 'Fullstack Dev Freelance',
                         posts_count: 0)
-    @post1 = Post.create(author_id: @user.id, Title: 'My First Post', text: 'This is the content of my first post.',
+    @post1 = Post.create(author_id: @user.id, title: 'My First Post', text: 'This is the content of my first post.',
                          likes_count: 0, comments_count: 0)
-    @post2 = Post.create(author_id: @user.id, Title: 'Rails Rocks', text: 'Excited about Rails development!',
+    @post2 = Post.create(author_id: @user.id, title: 'Rails Rocks', text: 'Excited about Rails development!',
                          likes_count: 0, comments_count: 0)
-    @post3 = Post.create(author_id: @user.id, Title: 'Learning Ruby', text: 'Exploring the beauty of Ruby language.',
+    @post3 = Post.create(author_id: @user.id, title: 'Learning Ruby', text: 'Exploring the beauty of Ruby language.',
                          likes_count: 0, comments_count: 0)
     visit user_path(@user.id)
   end
